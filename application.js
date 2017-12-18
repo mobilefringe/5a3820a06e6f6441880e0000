fa66bd566382f8ebaf2152a30b9ce9e340335916
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-12-18 15:34:45 -0500

    Cornwall 2.0 1.0  /application.js

--- application.js
+++ application.js
@@ -243,9 +243,9 @@ function renderHours(container, template, collection, type){
                 } else {
                     val.h = "Closed";
                 }
-                if (val.h != "Closed"){
+                // if (val.h != "Closed"){
                     item_list.push(val);
-                }
+                // }
             }
         });
         collection = [];
