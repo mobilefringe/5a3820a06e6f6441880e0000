5f7a0576a2fe7eadbd82b09194923f12f52f2242
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-12-18 18:09:13 -0500

    Cornwall 2.0 1.0  /application.js

--- application.js
+++ application.js
@@ -472,6 +472,8 @@ function renderPromotions(container, template, collection){
             val.dates = start.format("MMM D") + " - " + end.format("MMM D")
         }
         
+        if(start.format("DMY") == end.format("DMY")){
+        }
         if(val.description.length > 160){
             val.description_short = val.description.substring(0, 164) + "...";
         } else {
