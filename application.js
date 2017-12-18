08fc516af9efcfb654de5f2fbfc31ffe3de51f1a
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-12-18 18:21:28 -0500

    Cornwall 2.0 1.0  /application.js

--- application.js
+++ application.js
@@ -476,7 +476,7 @@ function renderPromotions(container, template, collection){
             val.days_left = "SALE ENDS TODAY";
             console.log(val.days_left);
         }
-        else if (end.diff(today, 'days') < 5) {
+        else if (end.diff(today, 'days',true) < 5) {
             val.days_left = "SALE ENDS in " + end.diff(today, 'days');
              console.log(today.format("DMY") , end.format("DMY"), today.format("DMY") == end.format("DMY"))
             console.log(val.days_left);
