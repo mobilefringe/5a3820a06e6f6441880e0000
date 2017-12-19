758133119c4648081ffab9db43c1ca76f133c201
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-12-19 11:26:49 -0500

    Cornwall 2.0 1.0  /helper.js

--- helper.js
+++ helper.js
@@ -181,6 +181,7 @@ function store_search() {
     $('#search_icon').click(function(){
         $("#site_search").show();
         if ($('#site_search').val() == ""){
+            console.log("hey");
             $("#site_search").hide();
         }
         
