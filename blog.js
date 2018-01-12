1dc8dd4642712ac125abae1cc065c149194aa183
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-01-12 11:48:45 -0500

    Cornwall 2.0 1.0  /blog.js

--- blog.js
+++ blog.js
@@ -92,28 +92,28 @@ function show_content() {
     $("#content").css('visibility','visible').hide().fadeIn('slow');
     $(".loader_backdrop").remove();
     
-    var today_hours = getTodaysHours();
-    renderHomeHours('#home_hours_container', '#home_hours_template', today_hours);
-    renderHomeHours('#home_hours_container_footer', '#home_hours_template_footer', today_hours)
+    // var today_hours = getTodaysHours();
+    // renderHomeHours('#home_hours_container', '#home_hours_template', today_hours);
+    // renderHomeHours('#home_hours_container_footer', '#home_hours_template_footer', today_hours)
     
-    var hours = getPropertyRegularHours();
-    var hours_mf = [];
-    var hours_sat = [];
-    var hours_sun = [];
-    $.each(hours, function(key, val){
-        if (val.day_of_week == 1 && val.is_holiday == false){
-            hours_mf.push(val);
-        }
-        if (val.day_of_week == 6 && val.is_holiday == false){
-            hours_sat.push(val);
-        }
-        if (val.day_of_week == 0 && val.is_holiday == false){
-            hours_sun.push(val);
-        }
-    });
-    renderHours('#hours_mf_container','#hours_mf_template', hours_mf, 'reg_hours');
-    renderHours('#hours_sat_container','#hours_sat_template', hours_sat, 'reg_hours');
-    renderHours('#hours_sun_container','#hours_sun_template', hours_sun, 'reg_hours');
+    // var hours = getPropertyRegularHours();
+    // var hours_mf = [];
+    // var hours_sat = [];
+    // var hours_sun = [];
+    // $.each(hours, function(key, val){
+    //     if (val.day_of_week == 1 && val.is_holiday == false){
+    //         hours_mf.push(val);
+    //     }
+    //     if (val.day_of_week == 6 && val.is_holiday == false){
+    //         hours_sat.push(val);
+    //     }
+    //     if (val.day_of_week == 0 && val.is_holiday == false){
+    //         hours_sun.push(val);
+    //     }
+    // });
+    // renderHours('#hours_mf_container','#hours_mf_template', hours_mf, 'reg_hours');
+    // renderHours('#hours_sat_container','#hours_sat_template', hours_sat, 'reg_hours');
+    // renderHours('#hours_sun_container','#hours_sun_template', hours_sun, 'reg_hours');
 }
 
 function renderMainPost(container, template, val){
