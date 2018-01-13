9942dc7763406b5a53b19cc6e3506a220ffa33c6
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-01-12 19:58:53 -0500

    Cornwall 2.0 1.0  /blog.js

--- blog.js
+++ blog.js
@@ -68,6 +68,7 @@ function renderPostDetailData(){
 
         renderPostDetails("#blog_banner_container", "#blog_banner_template", post, blog_posts);
         renderPostDetails("#current_blog_container", "#current_blog_template", post, blog_posts);
+        var popular_blogs = blog_posts.sortBy(function(o){ return o.impression_count}).reverse();
         renderPosts('#popular_blog_container_1','#popular_blog_template_1', blog_posts);
     }
 function init() {
