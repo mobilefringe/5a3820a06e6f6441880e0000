6dafbcdba9cd4ce04113c0ba1a1e11c0de776a2e
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-01-12 20:33:45 -0500

    Cornwall 2.0 1.0  /blog.js

--- blog.js
+++ blog.js
@@ -5,7 +5,7 @@
 
 function renderPostsPageData(){
     
-    //check if tag is attached to path
+    //check if tag is attached to pathslug
     
     var blog_posts = getBlogDataBySlug('cornwall-main').posts.sortBy(function(o){ return o.publish_date}).reverse();
     // console.log(blog_posts);
