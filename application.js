fe9360f1d541d80a647ff907e05f12a3d1090198
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-01-26 15:02:13 -0500

    Cornwall 2.0 1.0  /application.js

--- application.js
+++ application.js
@@ -426,7 +426,7 @@ function renderLogoScroll(container, template, collection){
     var template_html = $(template).html();
     Mustache.parse(template_html); 
     $.each( collection , function( key, val ) {
-        console.log("default_image",default_image);
+        console.log("default_image",val.store_front_url);
         if(val.store_front_url != null || val.store_front_url.indexOf('missing.png') > -1){
             val.store_logo = getImageURL(val.store_front_url);
         } else {
