e25ee687526433b5c113a5cd2c23e4d4c5f70bab
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-01-26 15:01:41 -0500

    Cornwall 2.0 1.0  /application.js

--- application.js
+++ application.js
@@ -431,7 +431,7 @@ function renderLogoScroll(container, template, collection){
             val.store_logo = getImageURL(val.store_front_url);
         } else {
             val.store_logo = default_image;
-            console.log("default_image",default_image);
+            console.log("default_image 2",default_image);
         }
         var repo_rendered = Mustache.render(template_html,val);
         item_rendered.push(repo_rendered);
