b8b820a0bf50ee742cd248909f52a5cdc5469e10
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-03-13 17:47:02 -0400

    Cornwall 2.0 1.1  /application.js

--- application.js
+++ application.js
@@ -592,7 +592,7 @@ function renderPromoDetails(container, template, collection){
                 val.image_url = val.promo_image_url_abs;
             }
         }
-        console.log("val.promo_image_url_abs", val.promo_image_url_abs);
+        console.log("val.promo_image_url_abs", val.promo_image_url_abs, val.promo_image_url_abs.indexOf('missing.png') > 0);
         if(val.promo_image_url_abs.indexOf('missing.png') > 0){
             // val.store_image_url = default_image.image_url;
             $(".promo_details_img").hide();
