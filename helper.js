b1796c7032a09644e86da1aa053a50fcd35faa68
irene@mobilefringe.com @ CodeCloud <irene@mobilefringe.com @ CodeCloud>
2019-12-03 11:12:23 -0500

    Cornwall 2.0 1.2  /helper.js

--- helper.js
+++ helper.js
@@ -41,32 +41,6 @@ function init() {
 //     $('#instafeed').html(data)
 // }
 
-function detectIE() {
-        var ua = window.navigator.userAgent;
-    
-        var msie = ua.indexOf('MSIE ');
-        if (msie > 0) {
-            // IE 10 or older => return version number
-            return parseInt(ua.substring(msie + 5, ua.indexOf('.', msie)), 10);
-        }
-    
-        var trident = ua.indexOf('Trident/');
-        if (trident > 0) {
-            // IE 11 => return version number
-            var rv = ua.indexOf('rv:');
-            return parseInt(ua.substring(rv + 3, ua.indexOf('.', rv)), 10);
-        }
-    
-        var edge = ua.indexOf('Edge/');
-        if (edge > 0) {
-           // Edge (IE 12+) => return version number
-           return parseInt(ua.substring(edge + 5, ua.indexOf('.', edge)), 10);
-        }
-    
-        // other browser
-        return false;
-    }
-
 function show_content() {
     $("#content").css('visibility','visible').hide().fadeIn('slow');
     $(".loader_backdrop").remove();
@@ -443,6 +417,32 @@ function truncateWords(str, no_words) {
         return str.splice(0, no_words).join(" ");
     }
     
+}
+
+function detectIE() {
+    var ua = window.navigator.userAgent;
+
+    var msie = ua.indexOf('MSIE ');
+    if (msie > 0) {
+        // IE 10 or older => return version number
+        return parseInt(ua.substring(msie + 5, ua.indexOf('.', msie)), 10);
+    }
+
+    var trident = ua.indexOf('Trident/');
+    if (trident > 0) {
+        // IE 11 => return version number
+        var rv = ua.indexOf('rv:');
+        return parseInt(ua.substring(rv + 3, ua.indexOf('.', rv)), 10);
+    }
+
+    var edge = ua.indexOf('Edge/');
+    if (edge > 0) {
+       // Edge (IE 12+) => return version number
+       return parseInt(ua.substring(edge + 5, ua.indexOf('.', edge)), 10);
+    }
+
+    // other browser
+    return false;
 }
 
 function getAssetURL(id){
